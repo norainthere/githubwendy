d80556f62f1b33211fbcbcd597659de41c312973
norainthere <129417183+norainthere@users.noreply.github.com>
2023-05-01 19:28:51 -0400

    streamlit dependency

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from transformers import GPT2Tokenizer, GPT2LMHeadModel
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 # Set up OpenAI Client
+openai.api_key = st.secrets["openai"]["openai_api_key"]
 openai_model = 'text-davinci-003'
 
 # Set up OpenAI Playground Trained Model
